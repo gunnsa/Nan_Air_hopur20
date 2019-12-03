a25978252859966f183af6a0171062b09f69b2e4
Gunnsa <gunnhildur19@ru.is>
2019-12-03 18:01:07 +0000

    changes on employee, undinished though

--- employee.py
+++ employee.py
@@ -9,7 +9,8 @@ class Employee:
         self.phonenumber = phonenumber
 
 
-class EmployeeIO:    
+class EmployeeIO:
+    # all_emps = []
     def get_emp_from_file():
         path = "Crew.csv"
         with open(path, "r", encoding="utf-8") as crew_file:
@@ -20,10 +21,38 @@ class EmployeeIO:
                 emp = Employee(line[0], line[1], line[2], line[3], line[4], line[5], line[6])
                 print(line)
                 all_emps.append(emp)
+        all_pilots = []
+        all_cabincrew = []
+        for employee in all_emps:
+            # print(line)
+            if employee.role == 'Pilot':
+                all_pilots.append(employee.name)
+            else:
+                all_cabincrew.append(employee.name)
+        print('cabin crew:', all_cabincrew)
+        print('pilots: ', all_pilots)
         return all_emps
+
+# class EmployeeFilter():
+#     def __init__(self, all_emps):
+#         self.all_emps = all_emps
+
+#     def list_role(self):
+#         all_pilots = []
+#         all_cabincrew = []
+#         for line in all_emps:
+#             print(line)
+#             if line[2] == 'Pilot':
+#                 all_pilots.append(line[1])
+#             else:
+#                 all_cabincrew.append(line[1])
+#         print(all_cabincrew)
+#         print(all_pilots)
+
 
 
 if __name__ == "__main__":
     emps = EmployeeIO.get_emp_from_file()
-    for employee in emps:
-        print(employee.name)
+    # roles = EmployeeFilter.list_role(emps)
+    # for employee in emps:
+        # print(employee.ssn)
