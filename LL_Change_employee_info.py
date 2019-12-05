a6113659e1d24194599cf75dca582d47d6ce2af6
veronikasif <veronikas19@ru.is>
2019-12-05 00:26:22 +0000

    ;)

--- LL_Change_employee_info.py
+++ LL_Change_employee_info.py
@@ -16,8 +16,8 @@ class Change_Employee_information:
 
         print('{}\n{:^44}\n{}'.format((Change_Employee_information.HEADER*Change_Employee_information.MAX), 'Change employee information', (Change_Employee_information.HEADER*Change_Employee_information.MAX)))
         
-        print('\n\tP - Print list of all Cabin crew',
-        '\n\tF - Find by specific condition\n')
+        print('\n\tN - Find employee by name',
+        '\n\tS - Find employee by social security number\n')
 
         print('\n{:<15}{:^14}{:>15}'.format(Change_Employee_information.Q, Change_Employee_information.M, Change_Employee_information.B))
         print(Change_Employee_information.CHOOSE*Change_Employee_information.MAX)
@@ -25,5 +25,22 @@ class Change_Employee_information:
         print()
         return command
 
+
+    def cabin_command(self):
+        ''' Says where to go according to input command '''
+
+        while self != 'Q':
+
+            if self == 'N':
+                Find_employee_by_name = 0
+
+            elif self == 'S':
+                Find_employee_by_ssn = 0
+
+            else:
+                print("Invalid command")
+                self = input('Please enter command: ').upper()
+
 if __name__ == "__main__":
     employee_info_command = Change_Employee_information.Change_employee_information()
+    cabin_command(employee_info_command)
