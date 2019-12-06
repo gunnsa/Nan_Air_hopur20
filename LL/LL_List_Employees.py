8967886175936c63c4f2da78889aaa3e36f076ab
monikasol <monika18@ru.is>
2019-12-06 14:13:10 +0000

    lista pilot og cabin

--- LL/LL_List_Employees.py
+++ LL/LL_List_Employees.py
@@ -6,6 +6,25 @@ class LL_List_Employees:
         crew_list = IO_EmployeesFromFile().get_emp_from_file()
         return crew_list
 
+    def get_Pilots(self, pilot):
+        allCrew = self.get_crew_list()
+        retList = []
+        for PilotMember in allCrew:
+            if pilot == PilotMember.role:
+                retList.append(PilotMember.name)
+                retList.append(PilotMember.rank)
+        return retList
+
+    def get_Cabin(self, cabin):
+        allCrew = self.get_crew_list()
+        retList = []
+        for CabinMember in allCrew:
+            if cabin== CabinMember.role:
+                retList.append(CabinMember.name)
+                retList.append(CabinMember.rank)
+        return retList  
+
+
     def getOneEmployee(self, userName):
         allCrew = self.get_crew_list()
         retList = []
